ff299a80d1fad9368068ea24f686140fa9ec1b77
DevTriangle <halbert370@gmail.com>
2024-05-20 12:03:59 +0300

    Добавлены сиды кармы

---
+++ src/db/seeds/1716195607549-SeedKarma.ts
@@ -0,0 +1,33 @@
+import { FormulaTypesEnum } from 'src/common/constants/constants'
+import { FormulaResult } from 'src/modules/formula-result/entities/formula-result.entity'
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class SeedKarma1716195607549 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    //КАРМА
+    for (let index = 1; index <= 22; index++) {
+      await queryRunner.manager.insert(FormulaResult, [
+        {
+          formula_type_id: FormulaTypesEnum.KARMA,
+          result_keys: [index.toString()],
+          result_name: index.toString(),
+          result_content: '',
+          language_code: 'ru',
+        },
+        {
+          formula_type_id: FormulaTypesEnum.KARMA,
+          result_keys: [index.toString()],
+          result_name: index.toString(),
+          result_content: '',
+          language_code: 'en',
+        },
+      ])
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.delete(FormulaResult, {
+      formula_type_id: FormulaTypesEnum.KARMA,
+    })
+  }
+}
